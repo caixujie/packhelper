99cd10df55513309bf2c391314398b85bcf9a7d2
swegnhan <1093541368@qq.com>
2019-01-18 14:19:59 +0800

    2019.1.18 commit

--- src/main/java/pres/swegnhan/packhelper/core/Package.java
+++ src/main/java/pres/swegnhan/packhelper/core/Package.java
@@ -1,8 +1,13 @@
 package pres.swegnhan.packhelper.core;
 
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
--- src/test/java/pres/swegnhan/packhelper/api/PackageApiTest.java
+++ src/test/java/pres/swegnhan/packhelper/api/PackageApiTest.java
@@ -1,4 +1,54 @@
 package pres.swegnhan.packhelper.api;
 
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.hamcrest.core.IsEqual;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import pres.swegnhan.packhelper.application.commandservice.PackageCommandService;
+import pres.swegnhan.packhelper.core.Package;
+
+@WebMvcTest({PackageApi.class})
 public class PackageApiTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private PackageCommandService packageCommandService;
+
+    private Package pack;
+
+    @Before
+    public void setUp() throws Exception{
+        RestAssuredMockMvc.mockMvc(mvc);
+        pack = new Package(
+                "apache2",
+                "2.4.29-1ubuntu4.5",
+                "1",
+                "",
+                ".deb",
+                new String[]{}
+        );
+    }
+
+    @Test
+    @Rollback
+    @Transactional
+    public void should_create_package_success() throws Exception{
+        RestAssuredMockMvc.given().
+                param("name", pack.getName()).
+                param("version", pack.getVersion()).
+                param("category", pack.getCategory()).
+                param("filetype", pack.getFiletype()).
+                param("supsList", pack.getSupsList()).
+                when().post("/package").
+                then().statusCode(200).body("content", "");
+    }
+
 }
